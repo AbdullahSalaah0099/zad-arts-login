3f906d35346b4f47a81ec8c6138771358bea6905
Abdullah Salah <95449686+AbdullahSalaah0099@users.noreply.github.com>
2022-04-03 00:39:12 +0200

    Add files via upload

---
+++ زاد.py
@@ -0,0 +1,367 @@
+import requests,time, sys,os ,webbrowser ,pyfiglet 
+webbrowser.open('https://t.me/Techno0099')
+time.sleep (1)
+poli=pyfiglet.figlet_format ('ZAD ARTS')
+print ('\033[;096m'+poli)
+c=('\033[;092m×××××××××××××××××××××××××××××××××××××××')
+for I in c+'\n':
+	sys.stdout.write (I)
+	sys.stdout.flush ()
+	time.sleep (00.050)
+print ()
+d =('\033[;092m##### Welcome To Abdullah Script #####')
+for I in d +'\n':
+	sys.stdout.write (I)
+	sys.stdout.flush ()
+	time.sleep (00.080)
+print ()
+c='###Script Solve Zad website errors###'
+for I in c+'\n':
+    sys.stdout.write(I) 
+    sys.stdout.flush ()
+    time.sleep (00.080)
+print ()
+c='### Projected By Abdullah  Salah ###'
+for I in c+'\n':
+    sys.stdout.write(I) 
+    sys.stdout.flush ()
+    time.sleep (00.080)
+print ()
+
+
+i='''\033[;092m Telegram channel : 
+
+  ◇ http://t.me/Techno0099
+          
+Youttube channel  : 
+
+ ◇ https://youtube.com/channel/UCAbtkFAe9yyX0HJNFXyKJUg '''
+for I in i +'\n':
+	sys.stdout.write (I)
+	sys.stdout.flush ()
+	time.sleep (00.030)
+print ()
+print ('\033[;091m×'*37)
+print ()
+time.sleep (1)
+
+print ('\033[;093m》FIRST STEP《')
+#print ('👇👇👇👇👇👇')
+print ()
+#print ()
+Login_UserName_TB=input ('\033[;093m》\033[;092mEnter Your UserID  :  \033[;096m')
+print ()
+
+SecurityQuestion=input('\033[;093m》\033[;092mEnter Your Card Number  :  \033[;096m')
+print ()
+
+NewPassword=input ('\033[;093m》\033[;092mEnter Your New Password   :   \033[;096m')
+print ()
+
+confirmpassword=input ('\033[;093m》\033[;092mConfirm Your New Password  :  \033[;096m')
+print ()
+
+url='http://193.227.46.42/ZAD_ART/User_Interface/Login.Mobile.aspx'
+
+headers ={
+'Host': '193.227.46.42',
+
+'Connection': 'keep-alive',
+
+'Content-Length': '1495',
+
+'Cache-Control': 'max-age=0',
+
+'Origin': 'http://193.227.46.42',
+
+'Upgrade-Insecure-Requests': '1',
+
+'DNT': '1',
+
+'Content-Type': 'application/x-www-form-urlencoded',
+'User-Agent': 'Mozilla/5.0 (Linux; Android 11; SM-A225F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.85 Mobile Safari/537.36',
+'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+
+'Referer': 'http://193.227.46.42/ZAD_ART/User_Interface/Login.Mobile.aspx',
+
+'Accept-Encoding': 'gzip, deflate',
+
+'Accept-Language': 'ar-EG,ar-AE;q=0.9,ar;q=0.8,en-GB;q=0.7,en;q=0.6,en-US;q=0.5',
+
+'Cookie': 'ASP.NET_SessionId=rurh4ro0mdnv3ynrdu4onv0x'
+}
+
+data={
+'__LASTFOCUS':'',
+
+'__EVENTTARGET':'',
+
+'__EVENTARGUMENT':'',
+
+'__VIEWSTATE':'/wEPDwULLTE3Njk0MzY3NzUPFgIeE1ZhbGlkYXRlUmVxdWVzdE1vZGUCARYCAgMPZBYCAgEPZBYEZg9kFgJmD2QWBAIBD2QWCGYPZBYCZg9kFgICAQ8PFgIeCEltYWdlVXJsBUF+L1VwbG9hZC9DT19DT01QQU5ZLzEwMDAwMDAwMDAwLzIwMjEvMTIvNi9JTUctMjAyMTEyMDYtV0EwMDExLmpwZ2RkAgEPZBYCZg9kFgICAQ8PFgIeBFRleHQFLNis2KfZhdi52Kkg2LPZiNmH2KfYrCAg2YPZhNmK2Kkg2KfZhNin2K/Yp9ioZGQCAg9kFgJmD2QWAgIBDxYCHgdWaXNpYmxlaBYCAgEPZBYCZg9kFgICAQ8PFgIfAgUMMTMwNDEyMDAzMjE5ZGQCBA9kFgJmD2QWAgIBDxYCHwNnFgQCAQ9kFgJmD2QWAgIBDw8WAh8CBQwxMzA0MTIwMDMyMTlkZAIED2QWAmYPZBYCAgEPDxYCHwIFOtmF2Kcg2YfZiCDYsdmC2YUg2KrYrdmC2YrZgiDYp9mE2LTYrti12YrYqSDYp9mE2K7Yp9i1INio2YNkZAIDD2QWAgIBD2QWAmYPZBYEZg9kFgICAQ8PFgIfA2dkZAICD2QWAgIBDw8WAh8DaGRkAgEPZBYCZg9kFgJmDw8WAh8CBVvYrNmF2YrYuSDYp9mE2K3ZgtmI2YIg2YXYrdmB2YjYuNipIC0g2YbYuNin2YUg2KfZhNis2KfZhdi52Kkg2KfZhNiw2YPZitipICjYstin2K8pINuy27DbstuyZGQYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgUFDEltYWdlQnV0dG9uMgULVGl0bGVfRW50ZXIFElRpdGxlX0VkaXRQYXNzd29yZAUPVGl0bGVfQWRtaXNzaW9uBQ1UaXRsZV9TdXBwb3J0UmAz6LhlxWk8W66SVt1JeIpUvSHx/1ZX5e/L6KFr16g=',
+
+'__VIEWSTATEGENERATOR':'391BF263',
+
+'__PREVIOUSPAGE':'0bN3fiMzNKurBJ3sSAJT6iTg3UMcjYkrrmywWHSBN5mh1Dx2yzq7e3uWh_35_1MGnxZooatM4t2oESHCEndrn0lXJgvZqPR6NApjODf-FMOQN-kM3KK-2H7o3Sgyv46FnSqtXUy5r6edS0R6FP0rPw2',
+
+'Login_UserName_TB':Login_UserName_TB,
+
+'Login_Password_TB':'',
+
+'ForgetPassword_SecurityQuestionAnswer_TB':SecurityQuestion,
+
+'ForgetPassword_NewPassword_TB':NewPassword,
+
+'ForgetPassword_ConfirmNewPassword_TB':confirmpassword,
+
+'ImageButton2.x':'24',
+
+'ImageButton2.y':'18',
+
+'ConfirmUserControl1$IsVisible':'0'
+}
+r=requests.post (url,headers=headers, data=data).text
+
+#if r.find('عفوا, لا يوجد مستخدم بهذا الاسم'):
+#if  Login_UserName_TB != int:  
+#    print ('\033[;092mError User ')
+#    os.system ('exit')
+ #   exit()
+#if r.find('من فضلك أدخل اسم الدخول'):
+#if Login_UserName_TB!=int:    
+if len(Login_UserName_TB)!=12 or (Login_UserName_TB)==str:
+    print ('\033[;091mError User')
+    exit()
+#if 'اجابة ســـئوال السرية غير صحيحة'in r:
+elif  len(SecurityQuestion)!=14 or (SecurityQuestion)==str:
+    print ('\033[;091mError Card Number')
+    exit()
+#elif  'كلمة المرور يجب أن لا يقل طولها عن ثمانية أحرف' in r:
+elif len(NewPassword)<8:
+    print ('\033[;091mError, Password must be at least eight characters long')
+    exit()
+#elif 'كلمة المرور يجب أن تحتوي على أرقام و حروف'in r:
+elif (NewPassword)==str:
+    print ('\033[;091mError, Password must contain numbers and letters')
+    exit ()
+elif (confirmpassword)!=(NewPassword):
+    print ('\033[;091mError, New password and confirm password should be equal')
+    exit ()
+#elif 'كلمة المرور الجديدة وتأكيد كلمة المرور يجب أن يكونوا سواء'in r:
+
+#    print ('\033[;091mError in Password')
+    
+#    exit ()
+    
+else :
+    print ('\033[;096mDone Create ')
+    print ()
+time.sleep (1)
+print ()
+print ('\033[;091m×'*37)
+print ()
+
+#######################################
+print ('\033[;093m》SECOND STEP《')
+print ()
+#print ()
+time.sleep (1)
+User=input ('\033[;093m》\033[;092mEnter Your User  Login :\033[;096m')
+print ()
+
+Password=input ('\033[;093m》\033[;092mEnter Your Password  :\033[;096m')
+
+url2 ='http://193.227.46.42/ZAD_ART/User_Interface/Login.Mobile.aspx'
+
+
+headers2={
+#Login.Mobile.aspx HTTP/1.1
+
+'Host': '193.227.46.42',
+
+'Connection': 'keep-alive',
+
+'Content-Length': '1058',
+
+'Cache-Control': 'max-age=0',
+
+'Origin': 'http://193.227.46.42',
+
+'Upgrade-Insecure-Requests': '1',
+
+'DNT': '1',
+
+'Content-Type': 'application/x-www-form-urlencoded',
+
+'User-Agent': 'Mozilla/5.0 (Linux; Android 11; SM-A225F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.85 Mobile Safari/537.36',
+
+'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+
+'Referer': 'http://193.227.46.42/ZAD_ART/User_Interface/Login.Mobile.aspx',
+
+'Accept-Encoding': 'gzip, deflate',
+
+'Accept-Language': 'ar-EG,ar-AE;q=0.9,ar;q=0.8,en-GB;q=0.7,en;q=0.6,en-US;q=0.5',
+
+'Cookie': 'ASP.NET_SessionId=zpvxo0pt0ogdwg4xxc0av2b2'
+}
+
+data2={
+'__EVENTTARGET':"",
+
+'__EVENTARGUMENT':"",
+
+'__VIEWSTATE':'/wEPDwULLTE3Njk0MzY3NzUPFgIeE1ZhbGlkYXRlUmVxdWVzdE1vZGUCARYCAgMPZBYCAgEPZBYEZg9kFgJmD2QWAgIBD2QWBGYPZBYCZg9kFgICAQ8PFgIeCEltYWdlVXJsBUF+L1VwbG9hZC9DT19DT01QQU5ZLzEwMDAwMDAwMDAwLzIwMjEvMTIvNi9JTUctMjAyMTEyMDYtV0EwMDExLmpwZ2RkAgEPZBYCZg9kFgICAQ8PFgIeBFRleHQFLNis2KfZhdi52Kkg2LPZiNmH2KfYrCAg2YPZhNmK2Kkg2KfZhNin2K/Yp9ioZGQCAQ9kFgJmD2QWAmYPDxYCHwIFW9is2YXZiti5INin2YTYrdmC2YjZgiDZhdit2YHZiNi42KkgLSDZhti42KfZhSDYp9mE2KzYp9mF2LnYqSDYp9mE2LDZg9mK2KkgKNiy2KfYrykg27LbsNuy27JkZBgBBR5fX0NvbnRyb2xzUmVxdWlyZVBvc3RCYWNrS2V5X18WBQUIYnRuRW50ZXIFElRpdGxlX0VkaXRQYXNzd29yZAUUVGl0bGVfRm9yZ2V0UGFzc3dvcmQFD1RpdGxlX0FkbWlzc2lvbgUNVGl0bGVfU3VwcG9ydLgDT3QheQEGdpEk+PVVh/m8pMOP8Mr2g2olXZdXTQsD',
+
+'__VIEWSTATEGENERATOR':'391BF263',
+
+'__PREVIOUSPAGE':'0bN3fiMzNKurBJ3sSAJT6iTg3UMcjYkrrmywWHSBN5mh1Dx2yzq7e3uWh_35_1MGnxZooatM4t2oESHCEndrn0lXJgvZqPR6NApjODf-FMOQN-kM3KK-2H7o3Sgyv46FnSqtXUy5r6edS0R6FP0rPw2',
+
+'Login_UserName_TB':User,
+
+'Login_Password_TB':Password,
+
+'btnEnter.x':'19',
+
+'btnEnter.y':'30',
+
+'ConfirmUserControl1$IsVisible':'0'
+
+}
+print ()
+
+
+r2=requests.post(url2,headers=headers2,data=data2).text
+'''if r2.find (
+'كلمة المرور غير صحيحية , من فضلك أعد المحاولة , هذه كانت المحاولة رقم  ۱') :
+#    time.sleep (1)
+    print ('\033[;091mError Password')
+elif r2.find('كلمة المرور غير صحيحية , من فضلك أعد المحاولة , هذه كانت المحاولة رقم  ٣' ):
+#    time.sleep (1)
+    print ('\033[;091mError Password')
+elif r2.find('كلمة المرور غير صحيحية , من فضلك أعد المحاولة , هذه كانت المحاولة رقم  ٢'):
+    print ('\033[;091mError Password')
+elif r2.find( 'مرحبا بك على النظام , يرجى ادخال كلمة مرور جديدة و تأكيد كلمة المرور'):
+    print ('\033[;096mDone Login')
+    '''
+if (User)!=(Login_UserName_TB):
+    print ('\033[;091mError UserLogin')
+    exit ()
+elif (Password)!=(NewPassword):
+    print ('\033[;091mError Password')
+    exit()
+else :
+    time.sleep (1)
+    print ('\033[;096mDone Login')
+    time.sleep (1)
+    
+print ()
+os.system ('clear')
+time.sleep (1)
+#####################################
+
+print ('\033[;093m》LAST STEP《')
+print ()
+#print ()
+
+
+Login_UserName_TB9=input('\033[;093m》\033[;092mEnter Your UserLogin:  \033[;096m')
+print ()
+
+passwordxcv=input('\033[;093m》\033[;092mEnter New Password :  \033[;096m')
+print ()
+
+NewPashssworddjd=input ('\033[;093m》\033[;092mConfirm Your Password  :\033[;096m')
+print ()
+url3='http://193.227.46.42/ZAD_ART/User_Interface/Login.Mobile.aspx'
+
+headers3={
+'Host': '193.227.46.42',
+
+'Connection': 'keep-alive',
+
+'Content-Length': '1593',
+
+'Cache-Control': 'max-age=0',
+
+'Origin': 'http://193.227.46.42',
+
+'Upgrade-Insecure-Requests': '1',
+
+'DNT': '1',
+
+'Content-Type': 'application/x-www-form-urlencoded',
+
+'User-Agent': 'Mozilla/5.0 (Linux; Android 11; SM-A225F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.85 Mobile Safari/537.36',
+
+'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+
+'Referer': 'http://193.227.46.42/ZAD_ART/User_Interface/Login.Mobile.aspx',
+
+'Accept-Encoding': 'gzip, deflate',
+
+'Accept-Language': 'ar-EG,ar-AE;q=0.9,ar;q=0.8,en-GB;q=0.7,en;q=0.6,en-US;q=0.5',
+
+'Cookie': 'ASP.NET_SessionId=zpvxo0pt0ogdwg4xxc0av2b2'
+}
+
+data3={
+'__EVENTTARGET':'',
+
+'__EVENTARGUMENT':'',
+
+'__VIEWSTATE':'/wEPDwULLTE3Njk0MzY3NzUPFgIeE1ZhbGlkYXRlUmVxdWVzdE1vZGUCARYCAgMPZBYCAgEPZBYEZg9kFgJmD2QWAgIBD2QWCGYPZBYCZg9kFgICAQ8PFgIeCEltYWdlVXJsBUF+L1VwbG9hZC9DT19DT01QQU5ZLzEwMDAwMDAwMDAwLzIwMjEvMTIvNi9JTUctMjAyMTEyMDYtV0EwMDExLmpwZ2RkAgEPZBYCZg9kFgICAQ8PFgIeBFRleHQFLNis2KfZhdi52Kkg2LPZiNmH2KfYrCAg2YPZhNmK2Kkg2KfZhNin2K/Yp9ioZGQCAg9kFgJmD2QWAgIBD2QWCgIGD2QWAmYPZBYCAgEPDxYCHgdWaXNpYmxlZ2RkAgcPZBYCZg9kFgICAQ8PFgIfA2dkZAIID2QWAmYPZBYCAgEPDxYEHgdFbmFibGVkZx8DZ2RkAgkPZBYCZg9kFgQCAQ8PFgQfBGcfA2dkZAIDDw8WBB8EZx8DZ2RkAgoPZBYCZg9kFgICAQ8PFgQfBGcfA2dkZAIFD2QWAmYPZBYCAgEPZBYCZg9kFgQCAQ8PFgIfAQUYfi9JbWFnZXMvRXhjbGFtYXRpb24ucG5nZGQCAw8PFgYeCUZvcmVDb2xvcgolHwIFetmF2LHYrdio2Kcg2KjZgyDYudmE2Ykg2KfZhNmG2LjYp9mFICwg2YrYsdis2Ykg2KfYr9iu2KfZhCDZg9mE2YXYqSDZhdix2YjYsSDYrNiv2YrYr9ipINmIINiq2KPZg9mK2K8g2YPZhNmF2Kkg2KfZhNmF2LHZiNixHgRfIVNCAgRkZAIBD2QWAmYPZBYCZg8PFgIfAgVb2KzZhdmK2Lkg2KfZhNit2YLZiNmCINmF2K3ZgdmI2LjYqSAtINmG2LjYp9mFINin2YTYrNin2YXYudipINin2YTYsNmD2YrYqSAo2LLYp9ivKSDbstuw27LbsmRkGAEFHl9fQ29udHJvbHNSZXF1aXJlUG9zdEJhY2tLZXlfXxYFBQhidG5FbnRlcgUSVGl0bGVfRWRpdFBhc3N3b3JkBRRUaXRsZV9Gb3JnZXRQYXNzd29yZAUPVGl0bGVfQWRtaXNzaW9uBQ1UaXRsZV9TdXBwb3J0O5w40QKyLrwN/q8mEWyUc62qsDZ1aFyfSAlOF2synko=',
+
+'__VIEWSTATEGENERATOR':'391BF263',
+
+'__PREVIOUSPAGE':'0bN3fiMzNKurBJ3sSAJT6iTg3UMcjYkrrmywWHSBN5mh1Dx2yzq7e3uWh_35_1MGnxZooatM4t2oESHCEndrn0lXJgvZqPR6NApjODf-FMOQN-kM3KK-2H7o3Sgyv46FnSqtXUy5r6edS0R6FP0rPw2',
+
+'Login_UserName_TB':Login_UserName_TB9,
+
+'Login_Password_TB':passwordxcv,
+
+'FirstLogin_Password_TB':NewPashssworddjd,
+
+
+'btnEnter.x':'29',
+
+'btnEnter.y':'19',
+
+'ConfirmUserControl1$IsVisible':'1'
+
+}
+
+
+r3=requests.post (url3,headers=headers3,data=data3).text
+'''if r3.find ('كلمة المرور يجب أن لا يقل طولها عن ثمانية أحرف') :
+    print ('Password must be at least eight characters long')
+elif r3.find('كلمة المرور يجب أن تحتوي على أرقام و حروف'):
+    print ('Password must contain numbers and letters')
+elif r3.find('كلمة المرور الجديدة وتأكيد كلمة المرور يجب أن يكونوا سواء'):
+      print ('New password and confirm password should be equal')
+elif r3.find('لقد تم تعديل كلمة المرور بنجاح'):
+    time.sleep (1)
+    print ('\033[;096mDone Modified')'''
+if (Login_UserName_TB9)==str or len(Login_UserName_TB9)!=12:
+    print ('\033[;091mError User')
+elif len(passwordxcv)<8:
+    print ('\033[;091mError, Password must be at least eight characters long')
+#    exit()
+#elif 'كلمة المرور يجب أن تحتوي على أرقام و حروف'in r:
+elif (passwordxcv)==str:
+    print ('\033[;091mError, Password must contain numbers and letters')
+elif (passwordxcv)!=(NewPashssworddjd):
+    print ('\033[;091mError, New password and confirm password should be equal')
+    exit()
+else :
+    print ('\033[;096mDone Modified')
+    time.sleep (0.6)
+    print ()
+    print ()
+    print ('\033[;092m》Your User  is    :  '+'\033[;093m'+Login_UserName_TB9)
+    print ('\033[;092m》Your Password is :  '+'\033[;093m'+passwordxcv)
+#else:
+ #   print ('\033[;091mSomthing Wrong Try Again')
+ 
+ # Projected By Abdullah Salah 
+
